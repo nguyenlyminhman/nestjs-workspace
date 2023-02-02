cc9a12756b305308826a1269b9fc56790fc899d8
man.nguyen <lotte.dev@terralogic.com>
2023-02-02 09:29:09 +0700

    fix: prisma

--- prisma/src/main.ts
+++ prisma/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const dbService: PrismaService = app.get(PrismaService);
+  await dbService.enableShutdownHooks(app)
+  await app.listen(3000);  
 }
 bootstrap();
