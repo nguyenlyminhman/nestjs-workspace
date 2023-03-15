a9ec32f3e33b7a51aa8cbd004f1ac820ea586e35
man.nguyen <lotte.dev@terralogic.com>
2023-03-15 15:05:40 +0700

    feat: creat module

--- cqrs-saga/src/app.module.ts
+++ cqrs-saga/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './src/prisma/prisma.module';
+import { UserModule } from './src/modules/user/user.module';
+import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { OrderModule } from './modules/order/order.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
