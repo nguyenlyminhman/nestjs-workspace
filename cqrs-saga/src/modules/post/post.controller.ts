2e41ca1bdaf5f18da630cebb46cc5a0654d4e9d1
man.nguyen <lotte.dev@terralogic.com>
2023-03-17 17:00:29 +0700

    faet: done create post

--- cqrs-saga/src/modules/post/post.controller.ts
+++ cqrs-saga/src/modules/post/post.controller.ts
@@ -1,5 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
+import { ApiFoundResponse } from '@nestjs/swagger';
+import { CreatePostCommand } from './commands/create-post.command';
+import { CreatePostDto } from './dtos/create-post.dto';
+import { PostEntity } from './entity/post.entity';
 import { PostService } from './service/post.service';
 
 @Controller('post')
@@ -8,4 +12,14 @@ export class PostController {
         private readonly postService : PostService,
         private readonly commandBus : CommandBus,
     ){}
+
+    @Post()
+    @HttpCode(HttpStatus.OK)
+    @ApiFoundResponse({
+      description: 'create order',
+      type: PostEntity,
+    })
+    async create(@Body() createPostDto: CreatePostDto){      
+        return this.commandBus.execute(new CreatePostCommand(createPostDto))
+    }
 }
