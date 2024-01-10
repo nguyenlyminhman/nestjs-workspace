ed58293c0d1bd935e271254c06bbd93e4077fe8a
nguyenlyminhman <nguyenlyminhman@gmail.com>
2024-01-10 22:04:31 +0700

    feat: upload using aws-sdk

--- aws-s3/src/app.controller.ts
+++ aws-s3/src/app.controller.ts
@@ -1,12 +1,53 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import AWS from 'aws-sdk';
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+import config from './config.json'
 
-@Controller()
+
+@Controller('/s3')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/test')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/upload-sdk')
+  @UseInterceptors(FileInterceptor('media'))
+  uploadAwsSdk(@UploadedFile() media: Express.Multer.File) {
+    AWS.config.update({
+      accessKeyId: config.AWS_ACCESS_KEY,
+      secretAccessKey: config.AWS_SECRET_KEY,
+      region: config.REGION
+    });
+    
+    const s3 = new AWS.S3();
+
+    const key = media.originalname;
+
+    s3.putObject({
+      Bucket: config.BUCKET,
+      Body: media.buffer,
+      Key: key
+    })
+    .promise()
+    .then(res => {
+      console.log(`Upload succeeded - `, res);
+    })
+    .catch(err => {
+      console.log("Upload failed:", err);
+    });
+
+    return key;
+  }
+
+  @Post('/upload-client')
+  @UseInterceptors(FileInterceptor('media'))
+  uploadS3Client(@UploadedFile() media: Express.Multer.File): string {
+    console.log('file', media);
+    return null;
   }
 }
