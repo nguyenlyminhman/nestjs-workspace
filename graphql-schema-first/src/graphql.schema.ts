7fd1638853172fdd7eb63cd61c744fd8a988b5b8
nguyenlyminhman <nguyenlyminhman@gmail.com>
2024-02-13 06:13:57 +0700

    feat: crud category

--- graphql-schema-first/src/graphql.schema.ts
+++ graphql-schema-first/src/graphql.schema.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -8,34 +9,47 @@
 /* eslint-disable */
 
 export class CreateUserInput {
-  fullname?: Nullable<string>;
-  email?: Nullable<string>;
-  password?: Nullable<string>;
+    fullname?: Nullable<string>;
+    email?: Nullable<string>;
+    password?: Nullable<string>;
+}
+
+export class CreateCategoryInput {
+    name?: Nullable<string>;
 }
 
 export abstract class IQuery {
-  abstract users():
-    | Nullable<Nullable<User>[]>
-    | Promise<Nullable<Nullable<User>[]>>;
+    abstract users(): Nullable<Nullable<User>[]> | Promise<Nullable<Nullable<User>[]>>;
 
-  abstract user(id: string): Nullable<User> | Promise<Nullable<User>>;
+    abstract user(id: string): Nullable<User> | Promise<Nullable<User>>;
+
+    abstract categories(): Nullable<Nullable<Category>[]> | Promise<Nullable<Nullable<Category>[]>>;
+
+    abstract category(id: string): Nullable<Category> | Promise<Nullable<Category>>;
 }
 
 export abstract class IMutation {
-  abstract createUser(
-    createUserInput?: Nullable<CreateUserInput>,
-  ): Nullable<User> | Promise<Nullable<User>>;
+    abstract createUser(createUserInput?: Nullable<CreateUserInput>): Nullable<User> | Promise<Nullable<User>>;
+
+    abstract createCategory(createCategoryInput?: Nullable<CreateCategoryInput>): Nullable<Category> | Promise<Nullable<Category>>;
 }
 
 export abstract class ISubscription {
-  abstract userCreated(): Nullable<User> | Promise<Nullable<User>>;
+    abstract userCreated(): Nullable<User> | Promise<Nullable<User>>;
+
+    abstract categoryCreated(): Nullable<Category> | Promise<Nullable<Category>>;
 }
 
 export class User {
-  id: number;
-  fullname: string;
-  email: string;
-  password: string;
+    id: number;
+    fullname: string;
+    email: string;
+    password: string;
+}
+
+export class Category {
+    id: number;
+    name: string;
 }
 
 type Nullable<T> = T | null;
--- graphql-schema-first/src/modules/category/category.service.ts
+++ graphql-schema-first/src/modules/category/category.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { Category } from 'src/graphql.schema';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(readonly prismaService: PrismaService) {}
+
+  async findAll(): Promise<Category[]> {
+    return this.prismaService.category.findMany();
+  }
+
+  async findOneById(id: number): Promise<Category> {
+    return this.prismaService.category.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async create(category: CreateCategoryDto): Promise<Category> {
+    return this.prismaService.category.create({
+      data: {
+        ...category,
+      },
+    });
+  }
+}
