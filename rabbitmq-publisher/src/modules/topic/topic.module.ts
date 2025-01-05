eb983c5ceab141152d2cd981e02c96c75e3e6bf6
nguyenlyminhman <nguyenlyminhman@gmail.com>
2025-01-05 23:58:45 +0700

    rm service

--- rabbitmq-publisher/src/modules/topic/topic.module.ts
+++ rabbitmq-publisher/src/modules/topic/topic.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TopicController } from './topic.controller';
 import { TopicService } from './topic.service';
-import { PublishMessageService } from 'src/rabbitmq/publish-message.service';
 
 @Module({
   imports: [],
   controllers: [TopicController],
   providers: [
     TopicService,
-    PublishMessageService
   ]
 })
 export class TopicModule { }
