965efa75a65ddae59eba1bfc0253ffefba715351
nguyenlyminhman <nguyenlyminhman@gmail.com>
2025-01-06 23:51:12 +0700

    register Rbmq for each module

--- rabbitmq-publisher/src/modules/fanout/fanout.module.ts
+++ rabbitmq-publisher/src/modules/fanout/fanout.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FanoutController } from './fanout.controller';
 import { FanoutService } from './fanout.service';
+import { RbmqModule } from 'src/rbmq/rbmq.module';
+import { EFanout } from 'src/objects/enums/fanout.enum';
 
 @Module({
+  imports: [
+    RbmqModule.register({ queueName: EFanout.FANOUT_QUEUE }),
+  ],
   controllers: [FanoutController],
   providers: [FanoutService]
 })
