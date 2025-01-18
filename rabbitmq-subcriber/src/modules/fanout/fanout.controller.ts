8798d4a883fca310123b7aea79003a2201ab9abf
nguyenlyminhman <nguyenlyminhman@gmail.com>
2025-01-18 10:04:41 +0700

    fanout subcribe

--- rabbitmq-subcriber/src/modules/fanout/fanout.controller.ts
+++ rabbitmq-subcriber/src/modules/fanout/fanout.controller.ts
@@ -4,9 +4,14 @@ import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 @Controller('fanout')
 export class FanoutController {
     
-    @EventPattern('foex_queue_01')
+    @EventPattern('foex_queue')
+
     getMsg(@Payload() data: any, @Ctx() context: RmqContext) {
-      console.log('Fanout Queue: ', data);
+      console.log('Fanout Queue Handling 01: ', data);
     }
 
+    @EventPattern('foex_queue')
+    getMsgs(@Payload() data: any, @Ctx() context: RmqContext) {
+      console.log('Fanout Queue Handling 02: ', data);
+    }
 }
