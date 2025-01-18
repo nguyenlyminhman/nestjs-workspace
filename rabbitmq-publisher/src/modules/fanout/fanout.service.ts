54acca0fbb4818990c3dd842fea7a311156c7f65
nguyenlyminhman <nguyenlyminhman@gmail.com>
2025-01-18 10:04:26 +0700

    fanout push msg

--- rabbitmq-publisher/src/modules/fanout/fanout.service.ts
+++ rabbitmq-publisher/src/modules/fanout/fanout.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { EFanout, EFanoutPattern } from 'src/objects/enums/fanout.enum';
 
 @Injectable()
-export class FanoutService {}
+export class FanoutService {
+    constructor(
+        @Inject(EFanout.FANOUT_QUEUE) private rmq: ClientProxy,
+    ) { }
+
+    async pushMsgQ1() {
+        try {
+            return this.rmq.emit(EFanout.FANOUT_QUEUE, { msg: 'hello world from ' + EFanoutPattern.FANOUT_PATTERN_01 })
+        } catch (err) {
+            console.log(EFanoutPattern.FANOUT_PATTERN_01, err);
+            throw new BadRequestException(err);
+        }
+    }
+}
