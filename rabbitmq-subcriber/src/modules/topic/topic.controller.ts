91eec397ce7e2c012dcd2f79f16c8f552fc720b1
nguyenlyminhman <nguyenlyminhman@gmail.com>
2025-01-18 13:08:39 +0700

    topic subcribe

--- rabbitmq-subcriber/src/modules/topic/topic.controller.ts
+++ rabbitmq-subcriber/src/modules/topic/topic.controller.ts
@@ -1,12 +1,17 @@
 import { Controller } from '@nestjs/common';
-import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { Ctx, EventPattern, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
 
 @Controller('topic')
 export class TopicController {
 
-    @EventPattern('tpex_queue_01')
-    getMsg(@Payload() data: any, @Ctx() context: RmqContext) {
-      console.log('Topic Queue: ', data);
+    @MessagePattern('user.register.email') // Lắng nghe các routing key khớp với user.*.email
+    getMsgEmail(@Payload() data: any, @Ctx() context: RmqContext) {
+      console.log('User Email: ', data);
+    }
+
+    @EventPattern('user.order.success')
+    getMsgOrder(@Payload() data: any, @Ctx() context: RmqContext) {
+      console.log('User Order: ', data);
     }
 
 }
