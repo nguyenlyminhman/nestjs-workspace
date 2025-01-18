e25db5e708fd1aee71f659b1d60fddabbbf943e6
nguyenlyminhman <nguyenlyminhman@gmail.com>
2025-01-18 11:03:46 +0700

    Subcribe  header msg

--- rabbitmq-subcriber/src/modules/headers/headers.controller.ts
+++ rabbitmq-subcriber/src/modules/headers/headers.controller.ts
@@ -4,9 +4,14 @@ import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 @Controller('headers')
 export class HeadersController {
 
-    @EventPattern('hdex_queue_01')
+    @EventPattern({ headers: { format: 'pdf', type: 'report' } })
     getMsg(@Payload() data: any, @Ctx() context: RmqContext) {
-      console.log('Headers Queue: ', data);
+      console.log('Headers PDF Queue : ', data);
+    }
+    
+    @EventPattern({ headers: { format: 'json', type: 'report' } })
+    getMsgs(@Payload() data: any, @Ctx() context: RmqContext) {
+      console.log('Headers Json Queue: ', data);
     }
     
 }
